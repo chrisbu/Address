3ea52206907c932b6fd33409929837c00f070caa
Chris Buckett <chrisbuckett@gmail.com>
2012-10-14 21:31:42 +0100

    Updated

--- lib/Address.dart
+++ lib/Address.dart
@@ -10,8 +10,34 @@ abstract class AddressBase {
   String address2;
 
   get fullname => "$forename $surname";
+  
+  factory AddressBase() {
+    return new Address(); 
+  }
 }
 
+class Address implements AddressBase {
+  var _surname;
+  String get surname => _surname;
+  set surname(String value) {
+    if (value == null) {
+       throw new Exception("null surname is not allowed");
+    }
+    _surname = value;
+  }
+  
+  String forename = "";
+  String address1;
+  String address2;
+
+  get fullname => "$forename $surname";
+  
+  Address(this.surname) {}
+}
+
+
+
+
 log(AddressBase address) {
-  new Logger("Address").log(address);
+  new Logger("Address").log(Level.INFO, address);
 }
